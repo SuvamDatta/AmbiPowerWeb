48955079920d646df2119e4e16861adb58ac17b2
SuvamDatta <suvam.datta@ibm.com>
2023-06-10 16:44:23 +0530

    login implementation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,71 +1,115 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
+import $ from "jquery";
+
 function Login() {
-  // change
-  const [errorMessages, setErrorMessages] = useState({});
+  useEffect(() => {
+    $("#btnSendOtp").on("click", function () {
+      
+    });
+  }, []);
   const navigate = useNavigate();
-  const [mobile, setmobile] = useState("");
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-  const errors = {
-    otp: "Invalid otp",
-    mobile: "Invalid mobile",
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    var { mobileNumber, otp } = document.forms[0];
-    if (otp.value != 1234) {
-      // Invalid otp
-      setErrorMessages({ name: "otp", message: errors.otp });
+  const height = $(window).height();
+  const width = $(window).width();
+  const [txtPhNumber, setChangePhNo] = React.useState("");
+  const [txtOtp, setChangeOTP] = React.useState("");
+  const [txtUserName, setUserName] = React.useState("");
+  function handleUserNameChange(e) {
+    setUserName(e.target.value);
+  }
+  function handlenumberChange(e) {
+    setChangePhNo(e.target.value);
+  }
+  function handleotpChange(e) {
+    setChangeOTP(e.target.value);
+  }
+  function handleClick() {
+    if (txtUserName.length == 0) {
+      alert("Invalid User Name");
     } else {
-      setIsSubmitted(true);
-    }
-  };
-  function handleMobileChange(event) {
-    if (event.target.value.length > 10) {
-      setErrorMessages({ name: "MobileNo", message: errors.mobile });
-    } else {
-      setErrorMessages("");
+      if (txtPhNumber.length != 10) {
+        alert("Invalid Mobile Number");
+        setChangeOTP("");
+      } else {
+        if (txtOtp.length == 0) {
+          setTimeout(() => {
+            document
+              .getElementsByClassName("displaynone")[0]
+              .classList.remove("displaynone");
+            document
+              .getElementsByClassName("displaynone")[0]
+              .classList.remove("displaynone");
+            document
+              .getElementsByClassName("usernameform")[0]
+              .classList.add("displaynone");
+            document
+              .getElementsByClassName("mobileform")[0]
+              .classList.add("displaynone");
+              document.getElementsByClassName("submitbutton")[0].value="Submit OTP";
+          }, 1000);
+        } else if (txtOtp != "1234") {
+          alert("Invalid OTP");
+        } else {
+          navigate("/Home");
+        }
+      }
     }
   }
-  function handleOtpChange(event) {
-    setErrorMessages("");
-  }
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
+  return (
+    <div className="app">
+      <div className="topbend"></div>
+      <h2>Login</h2>
+      <div className="login-form">
+        <div style={{ textAlign: "center" }}>
+          <label className="otpsent displaynone">OTP Sent!</label>
+        </div>
+
+        <div className="input-container usernameform">
+          <label>User Name </label>
+          <input
+            type="text"
+            name="UserName"
+            onChange={(e) => {
+              handleUserNameChange(e);
+            }}
+          />
+        </div>
+
+        <div className="input-container mobileform">
           <label>Mobile Number </label>
           <input
             type="number"
             name="MobileNo"
-            required
-            onChange={handleMobileChange}
+            maxLength={10}
+            onChange={(e) => {
+              handlenumberChange(e);
+            }}
           />
-          {renderErrorMessage("MobileNo")}
         </div>
 
-        <div className="input-container">
+        <div className="input-container displaynone">
           <label>OTP </label>
-          <input type="number" name="otp" required onChange={handleOtpChange} />
-          {renderErrorMessage("otp")}
+          <input
+            id="txtotp"
+            type="number"
+            name="otp"
+            maxLength={4}
+            onChange={(e) => {
+              handleotpChange(e);
+            }}
+          />
         </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
 
-  return (
-    <div className="app">
-      <h2>Login</h2>
-      <div className="login-form">
-        {isSubmitted ? navigate("/Home") : renderForm}
+        <div className="button-container">
+          <input
+            type="submit"
+            id="btnSendOtp"
+            value="Send OTP"
+            onClick={handleClick}
+            className="submitbutton"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
 import './Sidebar.css';
-import { Link } from "react-router-dom";
-// ...
 export default props => {
   return (
-    <Menu>
-      <a className="menu-item"  href="/MyServices">
-        My Services
-      </a>
-      <a className="menu-item" href="/Driver">
-        Driver Page
-      </a>
-      <a className="menu-item" href="/Admin">
-        Admin
-      </a>
-      <a className="menu-item" href="/HelpAndSupport">
-        Help & Contact
-      </a>
-    </Menu>
+    <div></div>
+    // <Menu>
+    //   <a className="menu-item"  href="/MyServices">
+    //     My Services
+    //   </a>
+    //   <a className="menu-item" href="/Driver">
+    //     Driver Page
+    //   </a>
+    //   <a className="menu-item" href="/Admin">
+    //     Admin
+    //   </a>
+    //   <a className="menu-item" href="/HelpAndSupport">
+    //     Help & Contact
+    //   </a>
+    // </Menu>
   );
 };
